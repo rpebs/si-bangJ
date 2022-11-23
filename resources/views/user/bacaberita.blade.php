eddd67acace4c55d5e1f5d325d3915123c61a37d
Ramadhany <rpebspramesta@gmail.com>
2022-11-23 20:06:48 +0700

    update view titik

--- resources/views/user/bacaberita.blade.php
+++ resources/views/user/bacaberita.blade.php
@@ -2,11 +2,17 @@
 @section('content')
     <div class="container">
         <div class="row mt-5">
-            <h2 class="card-title">{{ $postingans->judul }}</h2>
-            <p class="card-text"><small class="text-muted">Di Posting pada : {{ $postingans->tgl_post }}</small></p>
-            <img src="/gambarpostingan/{{ $postingans->image }}" class="card-img-top" alt="...">
+            <div class="ms-auto me-auto col-md-8 justify-content-center">
+                <h2 class="card-title">{{ $postingans->judul }}</h2>
+                <p class="card-text"><small class="text-muted">Di Posting pada : {{ $postingans->tgl_post }}</small></p>
+                <div class="text-center">
+                    <img src="/gambarpostingan/{{ $postingans->image }}" alt="...">
+                </div>
 
-            <p class="card-text mt-5 mb-5">{{ $postingans->post }}</p>
+
+                <p class="card-text mt-5 mb-5">{{ $postingans->post }}</p>
+            </div>
+
 
 
         </div>
